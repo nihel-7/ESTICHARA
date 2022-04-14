5368e2085175b2e374e3d8f19549d60feee784bb
nardjess <64872078+nardjess@users.noreply.github.com>
2022-04-14 18:40:27 +0200

    Create home.blade.php

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      </head>
+        <body>
+          <style type="text/css">
+            tr:hover{background-color:#EEE;cursor: pointer}
+            li {
+                   
+                   display : inline;
+                   margin-left:10px;
+                   font-size: 17px;
+                   line-height: 40px;
+                   width: 180px;
+                   height: 40px;
+                   cursor: pointer;
+
+                }
+          </style>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Pharm-project</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                      <button class="btn btn-outline-primary" type="submit">sign out</button>
+                    
+                  </div>
+                </div>
+              </nav>
+      <h1> <center>home</center></h1>
+    
+</html>
