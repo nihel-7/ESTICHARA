67e6487af5fe436c9ee0768f2a40471910693ad7
nardjess <64872078+nardjess@users.noreply.github.com>
2022-04-14 18:32:51 +0200

    Update prescriptionanalysis.blade.php

--- resources/views/user/prescriptionanalysis.blade.php
+++ resources/views/user/prescriptionanalysis.blade.php
@@ -21,26 +21,10 @@
           </style>
             <nav class="navbar navbar-expand-lg navbar-light bg-light ">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Pharm-project</a>
+                  <a class="navbar-brand" href="/home">Pharm-project</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">prescription</a>
-                      </li>
-                     
-                      
-                    </ul>
-                   
-                      
-                      <button class="btn btn-outline-primary" type="submit">sign out</button>
-                    
-                  </div>
                 </div>
               </nav>
             
