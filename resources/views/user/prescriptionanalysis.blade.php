afcac8a385e7e016d9585ef32cdac121040ec44f
nardjess <64872078+nardjess@users.noreply.github.com>
2022-04-14 18:39:00 +0200

    Update prescriptionanalysis.blade.php

--- resources/views/user/prescriptionanalysis.blade.php
+++ resources/views/user/prescriptionanalysis.blade.php
@@ -21,7 +21,7 @@
           </style>
             <nav class="navbar navbar-expand-lg navbar-light bg-light ">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="/home">Pharm-project</a>
+                  <a class="navbar-brand" href="/">Pharm-project</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
