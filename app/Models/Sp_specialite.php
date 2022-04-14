766da7719046541d664b3cc5a26106418c6b96b2
nihel-7 <nihelbendimerad@gmail.com>
2022-04-14 20:22:58 +0200

    iujo

--- app/Models/Sp_specialite.php
+++ app/Models/Sp_specialite.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use DB;
 class Sp_specialite extends Model
-{
+{   
+    use HasFactory;
+    
     protected $table = 'sp_specialite';
     protected $primaryKey = 'SP_CODE_SQ_PK';
 }
