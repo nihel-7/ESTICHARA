de01eb9c87aa32548f09b928b91a04fe07ddd347
nardjess <benaribanana17@gmail.com>
2022-09-05 01:25:22 +0200

    medicamentdetails

--- resources/views/user/medicationdetail.blade.php
+++ resources/views/user/medicationdetail.blade.php
@@ -3,6 +3,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <!-- for icons plus minus font awesome-->
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -12,13 +19,81 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
         <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">                
       </head>
         <body>
+<style type="text/css">
+  body{
+  margin:0;
+  font-family: sans-serif;
+}
 
+*{
+  box-sizing: border-box;
+}
+
+.accordian{
+  max-width:700px;
+  display: block;
+  margin:100px auto;
+}
+
+.accordian .card{
+  box-shadow: 0px 0px 20px #d4d4d4;
+  margin-bottom:20px;
+  float: left;
+  width: 100%;
+}
+.accordian .card .card-header h3{
+  cursor: pointer;
+  color:#ffffff;
+  position: relative;
+  margin:0;
+  padding:5px 10px;
+
+}
+.accordian .card .card-header {
+  position: relative;
+}
+.accordian .card .card-header span{
+  position: absolute;
+  right:20px;
+  top:20px;
+  height:25px;
+  width:25px;
+  color:#0000FF;
+  background-color: #ffffff;
+  border-radius:50%;
+  text-align: center;
+  line-height:25px;
+  font-size:13px;
+}
+
+.accordian .card .card-body{
+  padding:20px;
+}
+.accordian .card .card-body{
+  display: none;
+}
+/*open one card by default*/
+.accordian .card:nth-child(1) .card-body{
+ display: block;
+}
+.accordian .card .card-body p{
+  font-size: 15px;
+  line-height: 24px;
+  color:#444444;
+  margin:0px;
+}
+
+
+
+
+
+
+
+
+</style>
             
             
               <section class="content-header">
@@ -31,6 +106,22 @@
                   </div>
                 </div><!-- /.container-fluid -->
               </section>
+             
+
+
+       
+
+
+
+<!-- jqurey code -->
+
+
+
+
+
+
+
+
 
           <!--info general-->
               <div class="container fluid">
@@ -66,45 +157,40 @@
             </div>
           </div>
            
-            </div>
+            
 
-              </div>
+             
 
               
               <!--details-->
 
 
-              <div class="container-fluid mt-5">
-                <div class="accordion accordion-flush " id="accordionExample">
-                  <div class="row">
-  <div class="accordion-item col-lg-6">
-    <h2 class="accordion-header card" id="flush-headingOne">
-      <div class="clearfix mb-0">
-      <button class="accordion-button card-header text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne" style=" background-color: rgba(252, 118, 8, 0.89);">
-        Modalité de prise
-      </button>
-    </h2>
-    <div id="flush-collapseOne" class="accordion-collapse collapse  " aria-labelledby="flush-headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body card-body">
-        @if(!$pos)
+
+
+
+  <div class="accordian">
+      <div class="card">
+          <div class="card-header" style=" background-color: rgba(252, 118, 8, 0.89);">
+             <h3>Modalité de prise</h3>
+             <span class="fa fa-minus"></span>
+          </div>
+          <div class="card-body active">
+                @if(!$pos)
                   <h5 class="card-title">Aucun</h5>
                   @else
                   <p class="card-text">{{$pos->ATR_TEXTE}}</p>
                   @endif
+          </div>
       </div>
-    </div>
-  </div></div>
-  
-              
-          <div class="accordion-item col-lg-6">
-    <h2 class="accordion-header card" id="flush-headingTwo">
-      <button class="accordion-button collapsed card-header text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo" style=" background-color:#07461a;">
-        Recommendation
-      </button>
-    </h2>
-    <div id="flush-collapseTwo" class="accordion-collapse collapse  " aria-labelledby="flush-headingTwo" data-bs-parent="#accordionExample">
-      <div class="accordion-body card-body">
-         @if($cat)
+
+      <div class="card">
+          <div class="card-header" style=" background-color:#07461a;">
+             <h3>Recommendation</h3>
+             <span class="fa fa-plus"></span>
+          </div>
+          <div class="card-body">
+             
+                  @if($cat)
                   {{$cat->FGATX9_TEXTE}}
                   @endif
                   @if(!$recs)
@@ -115,38 +201,30 @@
                   <p class="card-text">{{$rec->FCPMTX_TEXTE}}</p>
                   @endforeach
                   @endif
+          </div>
       </div>
-    </div>
-  </div>
-  <div class="accordion-item col-lg-6">
-    <div class="row">
-    <h2 class="accordion-header card" id="flush-headingthree">
-      <button class="accordion-button card-header text-white bg-warning" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsethree" aria-expanded="false" aria-controls="flush-collapsethree">
-        effet secondaire
-      </button>
-    </h2>
-    <div id="flush-collapsethree" class="accordion-collapse collapse  " aria-labelledby="flush-flush-headingthree" data-bs-parent="#accordionExample">
-      <div class="accordion-body card-body">
-         @if(!$eis)
+      <div class="card">
+          <div class="card-header bg-warning">
+             <h3>effet secondaire</h3>
+             <span class="fa fa-plus"></span>
+          </div>
+          <div class="card-body">
+               @if(!$eis)
                   <h5 class="card-title">Aucun</h5>
                   @else
                   @foreach($eis as $ei)
                   <p class="card-text">{{$ei->FEITX1_TEXTE}}</p>
                   @endforeach
                   @endif
+          </div>
       </div>
-    </div>
-  </div></div>
-              
-          <div class="accordion-item col-lg-6">
-    <h2 class="accordion-header card" id="flush-headingfour">
-      <button class="accordion-button collapsed card-header text-white bg-danger" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapsefour" aria-expanded="false" aria-controls="flush-collapsefour">
-        contre indication
-      </button>
-    </h2>
-    <div id="flush-collapsefour" class="accordion-collapse collapse  " aria-labelledby="flush-headingfour" data-bs-parent="#accordionExample">
-      <div class="accordion-body card-body">
-        @if(!$cis)
+      <div class="card">
+          <div class="card-header bg-danger">
+             <h3>contre indication</h3>
+             <span class="fa fa-plus"></span>
+          </div>
+          <div class="card-body">
+              @if(!$cis)
                 <h5 class="card-title">Aucun</h5>
                 @else
                 @foreach($cis2 as $ci2)
@@ -157,28 +235,12 @@
                 @endforeach 
                  
                 @endif
-      </div>
-    </div>
-  </div>
-        </div>
-       </div>
-     
-
-      <div class="container-fluid mt-5">
-        <div class="row justify-content-md-center mb-2">
-          <div class="col-md-12">
-          <div class="card ">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">Education therapeutique</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Text</small></p>
-            </div>
           </div>
-        </div> 
       </div>
-      
-      </div></div>
+  </div>
+  </div>
+
+             
 
 
       
@@ -187,6 +249,28 @@
                     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
                   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script>
+  $(document).ready(function(){
+    $(".card-header").click(function(){
+       // self clicking close
+       if($(this).next(".card-body").hasClass("active")){
+         $(this).next(".card-body").removeClass("active").slideUp()
+        $(this).children("span").removeClass("fa-minus").addClass("fa-plus")  
+       }
+     else{
+     $(".card .card-body").removeClass("active").slideUp()
+     $(".card .card-header span").removeClass("fa-minus").addClass("fa-plus");
+       $(this).next(".card-body").addClass("active").slideDown()
+        $(this).children("span").removeClass("fa-plus").addClass("fa-minus")
+      }
+    })
+  })
+
+</script>
+
         </body>
     
 </html>
